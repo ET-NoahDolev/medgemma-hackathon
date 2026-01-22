e1863a455210039f5b4e56ff9223015189515afd
Noah Dolev <noah@elixirtrials.com>
2026-01-22 17:41:48 +0100

    feat(inference): add Pydantic schemas for MedGemma tool outputs

--- components/inference/src/inference/__init__.py
+++ components/inference/src/inference/__init__.py
@@ -3,6 +3,18 @@
 from inference.agent_factory import create_react_agent
 from inference.config import AgentConfig
 from inference.model_factory import create_model_loader
+from inference.schemas import (
+    CriterionClassificationResult,
+    FieldMappingResult,
+    MedicalConceptsResult,
+)
 
-__all__ = ["AgentConfig", "create_model_loader", "create_react_agent"]
+__all__ = [
+    "AgentConfig",
+    "create_model_loader",
+    "create_react_agent",
+    "FieldMappingResult",
+    "CriterionClassificationResult",
+    "MedicalConceptsResult",
+]
 
